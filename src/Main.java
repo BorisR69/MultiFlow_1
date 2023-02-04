5b6fe7b8b946b62012dd29f5854afe47ac2eab2c
Boris Rozov <outsource@list.ru>
2023-02-04 22:07:44 +0300

    Ветка MAX на проверку

--- src/Main.java
+++ src/Main.java
@@ -6,16 +6,14 @@ public class Main {
     public static void main(String[] args) throws InterruptedException, ExecutionException {
         String[] texts = new String[25];
         int max = 0;
-        for (int i = 0; i < texts.length; i++) {
-            texts[i] = generateText("aab", 30_000);
-        }
+        for (int i = 0; i < texts.length; i++) texts[i] = generateText("aab", 30_000);
 
         long startTs = System.currentTimeMillis(); // start time
-        List<FutureTask<Integer>> listFuture = new ArrayList<>();
-        ExecutorService threadPool = Executors.newFixedThreadPool(Runtime.getRuntime().availableProcessors());
+        List<Future<Integer>> listFuture = new ArrayList<>();
+        ExecutorService threadPool = Executors.newFixedThreadPool(5);
         for (String text : texts) {
             // Создаем задачу для запуска в потоке
-            Callable run = () -> {
+            Callable<Integer> run = () -> {
                 int maxSize = 0;
                 for (int i = 0; i < text.length(); i++) {
                     for (int j = 0; j < text.length(); j++) {
@@ -37,26 +35,18 @@ public class Main {
                 System.out.println(Thread.currentThread().getName() + " " + text.substring(0, 100) + " -> " + maxSize);
                 return maxSize;
             };
-            FutureTask<Integer> task = new FutureTask<Integer>(run); // Создаем поток
-            listFuture.add(task);
+            Future<Integer> future = threadPool.submit(run); // запуск задачи на выполнение в пуле потоков
+            listFuture.add(future); // добавление задачи в список
         }
 
-        for (FutureTask futureTask : listFuture) {
-            new Thread(futureTask).start();
+        for (Future<Integer> futureTask : listFuture) { // Поиск максимального интервала значений
+            int curMax = futureTask.get();
+            if (max < curMax) max = curMax;
         }
-
-        for (FutureTask futureTask : listFuture) {
-            int curMax = (Integer) futureTask.get();
-            if (curMax > max) {
-                max = curMax;
-            }
-        }
-
-        System.out.println("Max = " + max);
+        System.out.println("Максимальный интервала значений = " + max);
         long endTs = System.currentTimeMillis(); // end time
-
         System.out.println("Time: " + (endTs - startTs) + "ms");
-
+        threadPool.shutdown(); // закрытие пула потоков
     }
 
     public static String generateText(String letters, int length) {
